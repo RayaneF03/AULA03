6780ed0909c360cda1d9224b214f38094f8230e3
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2025-08-08 10:49:39 -0300

    Adiciona classe Aluno e exemplos com coleções em C#
    - Criada a classe `Aluno` no namespace `AULA03` com propriedades e construtor.
    - Adicionados exemplos de manipulação de `List`, `Dictionary`, `Queue`, `Stack` e `HashSet` em `Program.cs`.
    - Removido código antigo e substituído por implementações mais completas.
    - Melhorada a organização da saída no console e adicionados comentários explicativos.

---
+++ aluno.cs
@@ -0,0 +1,14 @@
+﻿namespace AULA03
+{
+    public class Aluno  // Classe Aluno
+    {
+        public Aluno(int codigo, string nome) // Metodo construtor com parametros
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+        }
+
+        public int Codigo { get; set; } // Atributos
+        public string? Nome { get; set; }
+    }
+}
